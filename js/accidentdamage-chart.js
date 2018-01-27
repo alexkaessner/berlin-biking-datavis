c402be7e08b01d628b771b044d75f0d151da8aea
Alexander Käßner <alexkaessner@me.com>
2018-01-27 13:57:21 +0100

    turned accident damage chart sideways

--- js/accidentdamage-chart.js
+++ js/accidentdamage-chart.js
@@ -1,15 +1,15 @@
 // set the dimensions and margins of the graph
-var AD_margin = {top: 10, right: 0, bottom: 20, left: 30},
+var AD_margin = {top: 0, right: 10, bottom: 20, left: 60},
     AD_width = 670 - AD_margin.left - AD_margin.right,
     AD_height = 300 - AD_margin.top - AD_margin.bottom;
 
-var AD_x = d3.scaleBand()
-		.rangeRound([0, AD_width])
+var AD_y = d3.scaleBand()
+		.rangeRound([AD_height, 0])
 		.paddingInner(0.05)
 		.align(0.1)
 
-var AD_y = d3.scaleLinear()
-		.rangeRound([AD_height, 0]);
+var AD_x = d3.scaleLinear()
+		.rangeRound([0, AD_width]);
 
 var AD_z = d3.scaleOrdinal()
 		.range(["#98abc5", "#6b486b", "#ff8c00"]);
@@ -30,8 +30,8 @@ d3.csv("../data/accidentdamage-chart.csv", function(d, i, columns) {
 
 	var AD_keys = data.columns.slice(1);
 
-	AD_x.domain(data.map(function(d) { return d.type }))
-	AD_y.domain([0, d3.max(data, function(d) { return d.total })]).nice();
+	AD_y.domain(data.map(function(d) { return d.type }))
+	AD_x.domain([0, d3.max(data, function(d) { return d.total })]).nice();
 	AD_z.domain(AD_keys);
 
 	AD_svg.append("g")
@@ -42,25 +42,17 @@ d3.csv("../data/accidentdamage-chart.csv", function(d, i, columns) {
 		.selectAll("rect")
 		.data(function(d) { return d; })
     .enter().append("rect")
-      .attr("x", function(d) { return AD_x(d.data.type); })
-      .attr("y", function(d) { return AD_y(d[1]); })
-      .attr("height", function(d) { return AD_y(d[0]) - AD_y(d[1]); })
-      .attr("width", AD_x.bandwidth());
+      .attr("y", function(d) { return AD_y(d.data.type); })
+      .attr("x", function(d) { return AD_x(d[0]); })
+      .attr("width", function(d) { return AD_x(d[1]) - AD_x(d[0]); })
+      .attr("height", AD_y.bandwidth());
 
 	AD_svg.append("g")
       .attr("class", "axis")
       .attr("transform", "translate(0," + AD_height + ")")
-      .call(d3.axisBottom(AD_x));
+      .call(d3.axisBottom(AD_x).ticks(null, "s"));
 
   AD_svg.append("g")
       .attr("class", "axis")
-      .call(d3.axisLeft(AD_y).ticks(null, "s"))
-    .append("text")
-      .attr("x", 2)
-      .attr("y", y(AD_y.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
-      .attr("fill", "#000")
-      .attr("font-weight", "bold")
-      .attr("text-anchor", "start")
-      .text("Population");
+      .call(d3.axisLeft(AD_y));
 });
