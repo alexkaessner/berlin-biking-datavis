561f139e83da37eab1b880a2f94d973cf0b99634
Alexander Käßner <alexkaessner@me.com>
2018-01-30 10:29:03 +0100

    reversed to newer bike paths types basemap

--- js/bike-paths-map.js
+++ js/bike-paths-map.js
@@ -2,11 +2,11 @@
 
 var bikePathsMap = L.map('bike-paths-map', {
   center: [52.520008, 13.404954],
-  zoom: 13,
+  zoom: 12,
   scrollWheelZoom: false
 });
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   maxZoom: 18
 }).addTo(bikePathsMap);
 
