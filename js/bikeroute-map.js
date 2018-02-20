362cd1ee9f7e4c631cc6db6dab09cdde2f6638ee
Jolanta <jolantapaliszewsks@Jolantas-MacBook-Pro-3.local>
2018-02-20 22:03:15 +0100

    add carto map loading

--- js/bikeroute-map.js
+++ js/bikeroute-map.js
@@ -11,7 +11,7 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Source: <a href="https://www.berlin.de/senuvk/verkehr/mobil/fahrrad/radrouten/de/gps_tracks/index.shtml">Berlin Open Data - Berlin.de</a>'
 }).addTo(bikeRouteMap);
 
-const bikePathsMapClient = new carto.Client({
+const bikeRouteMapClient = new carto.Client({
   apiKey: config.CARTO_API_KEY,
   username: config.CARTO_USERNAME
 });
