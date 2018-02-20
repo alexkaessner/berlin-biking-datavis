80a18693a790efd89c7853ff959061e050a9493b
Jolanta <jolantapaliszewsks@Jolantas-MacBook-Pro-3.local>
2018-02-21 00:23:39 +0100

    improved name of Route

--- js/bikeroute-map.js
+++ js/bikeroute-map.js
@@ -2,7 +2,7 @@
 
 var bikeRouteMap = L.map('bikeroute-map', {
   center: [52.520008, 13.404954],
-  zoom: 12,
+  zoom: 10.5,
   scrollWheelZoom: false
 });
 
@@ -25,7 +25,7 @@ const bikeRouteMapSource = new carto.source.SQL(`
 const bikeRouteMapStyle = new carto.style.CartoCSS(`
   #layer {
     line-width: 2;
-    line-color: ramp([name], (#5F4690, #1D6996, #38A6A5, #0F8554, #73AF48, #EDAD08, #E17C05, #CC503E, #94346E, #6F4070, #666666, #1758b9, #b98e17, #b917b9, #ff1a00, #00d1d1, #2d71bf), ("Europaradweg_r1_ost", "Europaradweg_r1_west", "Gatowroute_rr2", "Hellersdorf_route_rr8", "Hohenschoenhausen_route_rr7", "Mauerweg_1", "Mauerweg_2", "Mauerweg_3", "Nordspange_tr2", "Ostring_tr7", "Wannseeroute_rr1", "Teltow_route_rr12", "Suedspange_tr4", "Spandau_route_rr3", "Reinickendorf_route_rr5" , "Radfernweg_berlin_usedom", "Radfernweg_berlin_kopenhagen"), "=");
+    line-color: ramp([name], (#5F4690, #1D6996, #38A6A5, #0F8554, #73AF48, #EDAD08, #E17C05, #CC503E, #94346E, #6F4070, #666666, #1758b9, #b98e17, #b917b9, #ff1a00, #00d1d1, #2d71bf), ("Europaradweg Ost", "Europaradweg West", "Gatow", "Hellersdorf", "Hohenschoenhausen", "1. Mauerweg", "2. Mauerweg", "3. Mauerweg", "Nordspange", "Ostring", "Wannsee", "Teltow", "Suedspange", "Spandau", "Reinickendorf" , "Radfernweg: Berlin-Usedom", "Radfernweg: Berlin-Kopenhagen"), "=");
   }
 `);
 
