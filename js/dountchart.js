f77022573b5e1dbfa302c3d2d9f0c537a9ec484d
Alexander Käßner <alexkaessner@me.com>
2018-02-27 16:42:04 +0100

    improving typo & added more detailed legend to accident map

--- js/dountchart.js
+++ js/dountchart.js
@@ -1,24 +1,24 @@
 var data = [
     {
-        "str_lab": "Cycle Tracks/Paths",
+        "str_lab": "cycle tracks/paths",
         "num": 214,
         "info": "Cycle tracks are dedicated lanes separated from the road. Usually cycle tracks are located on the sidewalk. They are either shared with pedestrians, separated trough painted marks, structurally differentiated or separated.",
         "image": 'graphics/bikepath-types/radweg.svg'
     },
     {
-        "str_lab": "Bike Lanes",
+        "str_lab": "bike lanes",
         "num": 70,
         "info": "Bike lanes are on-road lanes marked with a solid line and a road sign. You have to use this lane as a bicyclist. Therefore all motorized traffic is excluded and not permitted to drive or park there.",
         "image": 'graphics/bikepath-types/radfahrstreifen.svg'
     },
     {
-        "str_lab": "Advisory Bike Lanes",
+        "str_lab": "advisory bike lanes",
         "num": 100,
         "info": "This bike lane is on the street and divided with a dashed line. Cars and busses are permitted to drive on the lane. Parking vehicles is not allowed.",
         "image": 'graphics/bikepath-types/schutzstreifen.svg'
     },
     {
-        "str_lab": "Bus Lanes",
+        "str_lab": "bus lanes",
         "num": 10,
         "info": "Bus lanes are special lanes only for busses and taxis. You are allowed to use this lane as a bicyclist, when marked with a extra road sign. Bus lanes can either have dashed or solid lines.",
         "image": 'graphics/bikepath-types/bussonderstreifen.svg'
